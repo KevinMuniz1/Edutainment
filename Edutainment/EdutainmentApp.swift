cedab88300be5b424b0b78819b8c25a9ac04843a
KevinMuniz1 <munizkevin@outlook.com>
2024-01-19 17:13:54 -0500

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Kevin Muniz on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
