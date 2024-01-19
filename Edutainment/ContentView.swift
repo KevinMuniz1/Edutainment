12d4ab25da16da54a830f8ffa2a77f2c0362af7b
KevinMuniz1 <munizkevin@outlook.com>
2024-01-19 18:02:03 -0500

    I have created the start page for the user for them to enter their settings before starting the game, once they are ready then they can click start and begin playing

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    @State private var appIsActive = false
+    @State private var timesTables = 6
+    @State private var questions = 10
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Edutainment")
+            .font(.title).bold()
+            .frame(maxWidth: .infinity)
+            .background(.blue)
+            .foregroundStyle(.white)
+        
+        if !appIsActive{
+            VStack(){
+                Image("bear")
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                    .padding()
+                    .rotation3DEffect(.degrees(animationAmount),axis: (x: 0, y: 1, z: 0))
+                    .animation(.bouncy(duration: 3.0), value: animationAmount)
+                VStack(spacing: 25){
+                    Text("Settings")
+                        .font(.system(size: 30)).bold()
+                        .foregroundStyle(.blue)
+                    Stepper("Select up to which time tables you would like to practice: \(timesTables)", value: $timesTables, in: 2...12)
+                        .padding()
+                    Stepper("How many questions would you like to answer? \(questions)", value: $questions, in: 5...20, step: 5)
+                        .padding()
+                    Button("Start Game"){
+                        withAnimation {
+                            appIsActive = true
+                        }
+                    }.frame(width: 125, height: 50)
+                        .background(.blue)
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                        .clipShape(.rect(cornerRadius: 10))
+                }
+                Image("giraffe")
+                    .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                    .padding()
+                    .rotation3DEffect(.degrees(animationAmount),axis: (x: 0, y: 1, z: 0))
+                    .animation(.bouncy(duration: 3.0), value: animationAmount)
+                Spacer()
+            }.onAppear {
+                animationAmount = 360.0
+            }
+        } else {
+            
         }
-        .padding()
+             
     }
 }
 
